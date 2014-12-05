78f5dead0e6782cf61bfd3639b3e6f91a5c9be05
Marcelo Perretta <mperretta@e-commla.com>
2014-12-04 21:41:28 -0300

    First commit. Added the model tu use

---
+++ TipCalculator/Model/TipCalculatorModel.swift
@@ -0,0 +1,50 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by Bubu on 12/4/14.
+//  Copyright (c) 2014 MaWaPe. All rights reserved.
+//
+
+import UIKit
+
+class TipCalculatorModel: NSObject {
+    var total: Double //Total despues de impuestos
+    var taxPct: Double //Porcentaje de impuesto sobre el subtotal
+    var subtotal: Double //Subtotal de la cuenta
+    {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    
+    init(total:Double, taxPct:Double) {
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+    func calcTipWithTipPct(tipPct:Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    
+    func returnPossibleTips() -> [Int: Double] {
+        
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        
+        // 2
+        var retval = Dictionary<Int, Double>()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            
+            println("Valor Intermedio \(intPct)")
+            // 3
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+        
+    }
+
+   
+}
