af808905d721c4fb84e37aaadc42aff3b86a1793
Marcelo Perretta <mperretta@e-commla.com>
2014-12-13 12:17:09 -0300

    Updated vc and connected with objects on view

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,6 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var totalTextField : UITextField!
+    @IBOutlet var taxPctSlider : UISlider!
+    @IBOutlet var taxPctLabel : UILabel!
+    @IBOutlet var resultsTextView : UITextView!
+    
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
+    
+    func refreshUI() {
+        // 1
+        totalTextField.text = String(format: "%0.2f", tipCalc.total)
+        // 2
+        taxPctSlider.value = Float(tipCalc.taxPct) * 100.0
+        // 3
+        taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%)"
+        // 4
+        resultsTextView.text = ""
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +36,13 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func calculateTapped(sender : AnyObject) {
+    }
+    @IBAction func taxPercentageChanged(sender : AnyObject) {
+    }
+    @IBAction func viewTapped(sender : AnyObject) {
     }
 
 
